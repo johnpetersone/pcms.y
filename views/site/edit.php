85f46dd363072162be85bb5f36607a358c409bc3
petersone <petersone@driftwood.btk.pte.hu>
2019-04-30 16:14:34 +0200

    Tue Apr 30 16:14:34 CEST 2019

--- views/site/edit.php
+++ views/site/edit.php
@@ -1,7 +1,7 @@
 <?php
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use dosamigos\ckeditor\CKEditor;
 
 $this->title = 'PCMS.Y';
 /* Switch layout to pcms*/
@@ -16,8 +16,8 @@ $this->context->layout = 'main.pcms.y.php';
 		
 		<?= $form->field($model, 'page')->textInput() ?>
 		<?= $form->field($model, 'title')->textInput() ?>
-		<?= $form->field($model, 'html')->textArea() ?>
-
+		<?= $form->field($model, 'html')->widget(CKEditor::className(), [ 'options' => ['rows' => 2], 'preset' => 'full' ]);	?>
+		
 		<div class="form-group">
 			<?= Html::submitButton($model->isNewRecord ? 'Létrehoz' : 'Ment', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
 		</div>
