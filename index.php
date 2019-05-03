09a6b66e9e9e4cac91251fbc23a85d924ecb366a
petersone <petersone@driftwood.btk.pte.hu>
2019-05-03 09:04:37 +0200

    Fri May  3 09:04:36 CEST 2019

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
-<STYLE>
-	DIV {
-		background: #FAA;
-		margin: 100px;
-		padding: 50px;
-		border-radius: 10px;
-		font-family: monospace;
-		font-size: 12pt;
-		color: red;
-	}
-</STYLE>
 <?
-	?><DIV><?
+	function showerror($text) {
+		?><STYLE>
+			DIV {
+				background: #FAA;
+				margin: 100px;
+				padding: 50px;
+				border-radius: 10px;
+				font-family: monospace;
+				font-size: 12pt;
+				color: red;
+			}
+		</STYLE><?
+		?><DIV><?=$text ?></DIV><?
+		die;
+	}
+
 	$config = require 'config/web.php';
 	$db = $config['components']['db'];
 
@@ -21,36 +25,36 @@
 	try {
 		$dbh = new PDO($db['dsn'], $db['username'], $db['password']);
 	} catch (Exception $e) { 
-		die($e->getMessage().' check config/db.php'); 
+		showerror($e->getMessage().' check config/db.php'); 
 	}
 	
 	# CHECK pages TABLE OR CREATE
 	if( ! $dbh->query('SELECT 1 FROM pages') ) { 
 		$result = $dbh->query('CREATE TABLE `pages` (`key` int(11) NOT NULL, `page` text NOT NULL, `title` text NOT NULL,`html` text NOT NULL,`description` text, `keywords` text) DEFAULT CHARSET=utf8mb4  ROW_FORMAT=COMPACT;'); 
-		if ( ! $result) die($dbh->errorInfo()[2].' can\'t create table, please drop PAGES table manually'); 
+		if ( ! $result) showerror($dbh->errorInfo()[2].' can\'t create table, please drop PAGES table manually'); 
 		
 		$result = $dbh->query('INSERT INTO `pages` (`key`, `page`, `title`, `html`, `description`, `keywords`) VALUES (1, "index", "index", "<h2>happy cmsing</h2>", "pcms", "pcms");'); 
-		if ( ! $result) die($dbh->errorInfo()[2].' can\'t insert fields, please drop PAGES table manually'); 
+		if ( ! $result) showerror($dbh->errorInfo()[2].' can\'t insert fields, please drop PAGES table manually'); 
 
 		$result = $dbh->query('ALTER TABLE `pages` ADD PRIMARY KEY (`key`);'); 
-		if ( ! $result) die($dbh->errorInfo()[2].' can\'t set primary key, please drop PAGES table manually'); 
+		if ( ! $result) showerror($dbh->errorInfo()[2].' can\'t set primary key, please drop PAGES table manually'); 
 
 		$result = $dbh->query('ALTER TABLE `pages` MODIFY `key` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;'); 
-		if ( ! $result) die($dbh->errorInfo()[2].' can\'t set key autoincrement, please drop PAGES table manually'); 
+		if ( ! $result) showerror($dbh->errorInfo()[2].' can\'t set key autoincrement, please drop PAGES table manually'); 
 	}	
 	
 	# CHECK FIELDS
 	if( ! $dbh->query('SELECT page, title, html, description, keywords FROM pages LIMIT 1') ) {
-		die($dbh->errorInfo()[2].' Malformed table, please drop PAGES table manually');
+		showerror($dbh->errorInfo()[2].' Malformed table, please drop PAGES table manually');
 	}
 	#uploads folder missing
 	if ( ! file_exists('uploads') ) {
-		if ( ! mkdir('uploads') ) die(getcwd().'/uploads folder missing. please create manually.');
+		if ( ! mkdir('uploads') ) showerror(getcwd().'/uploads folder missing. please create manually.');
 	}
-	chmod('uploads',0777);
-	if( ! (decoct(fileperms('uploads')%512) == '777')) die (getcwd().'/uploads folder permissions need to be 777. could you please maybe aaaaa, you know.');
+	@chmod('uploads',0777);
+	if( ! (decoct(fileperms('uploads')%512) == '777')) showerror (getcwd().'/uploads folder permissions need to be 777. could you please maybe aaaaa, you know.');
 	
 	
-	?></DIV><?
+
 	header('Location:web/page/index'); 
 ?>
